17b5d86b6960a3db8880f9354cb534f4dc4b299a
HaoweiFan0912 <fhw0912@gmail.com>
2024-09-22 15:04:31 -0400

    Package deleted.
    this test file will no longer using testthat package.

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -9,29 +9,25 @@
 
 #### Workspace setup ####
 library(tidyverse)
-library(testthat)
 
 #### Test data ####
 
 # select target data
-data <- simulated_data
+data <- read_csv("data/analysis_data/simulated_data.csv")
 
 # Test 1: Ensure year_month format is correct (YYYY-MM)
-test_that("year_month format is YYYY-MM", {
-  expect_true(all(grepl("^\\d{4}-\\d{2}$", data$year_month)))
-})
+all(grepl("^\\d{4}-\\d{2}$", data$year_month))
+
 
 # Test 2: Ensure year_month is in the right period.
-test_that("year_month format between 2007-3 and 2024-2", {
-  expect_true(all(ym(data$year_month) >= ym("2007-03") 
-                  & ym(data$year_month) <= ym("2024-02")))
-})
+all(ym(data$year_month) >= ym("2007-03") 
+    & ym(data$year_month) <= ym("2024-02"))
+
 
 # Test 3: Ensure all seasons are assigned correctly
-test_that("Seasons are correctly assigned", {
-  valid_seasons <- c("Spring", "Summer", "Autumn", "Winter")
-  expect_true(all(data$season %in% valid_seasons))  
-})
+valid_seasons <- c("Spring", "Summer", "Autumn", "Winter")
+all(data$season %in% valid_seasons)
+
 
 # Test 4: the actual season matches the expected season
 # Function to determine the correct season based on the month
@@ -47,31 +43,18 @@ get_season <- function(month) {
   }
 }
 # Test to check if the 'season' column matches the correct season
-test_that("Seasons are correctly assigned based on months", {
-  # Extract the month from the 'year_month' column
-  data <- data %>%
-    mutate(month = as.numeric(format(as.Date(paste0(year_month, "-01")), "%m")),
-           correct_season = sapply(month, get_season))
-  
-  # Check if the season is correctly assigned
-  expect_true(all(data$season == data$correct_season))
-})
+correct_season <- tibble(correct_season = sapply(data$month, get_season))
+all(correct_season$correct_season == data$season)
+
 
 # Test 5: Check for valid total decedents
-test_that("Total decedents column contains valid values", {
-  expect_true(all(data$total_decedents >= 0) & # is non-negative?
-                all(data$total_decedents == as.integer(data$total_decedents))) 
-  # is integer?
-})
+all(data$total_decedents >= 0 & # is non-negative?
+      data$total_decedents == as.integer(data$total_decedents)) # is integer?
 
-# Test 6: Check for valid 'male'
-test_that("male column contains valid values", {
-  expect_true(all(data$male >= 0) & # is non-negative?
-                all(data$male == as.integer(data$male))) # is integer?
-})
+# Test 6: Check for valid 'male'.
+all(data$male >= 0 & # is non-negative?
+      data$male == as.integer(data$male)) # is integer?
 
-# Test 7: Check that the total_decedents column matches male + female
-test_that("total_decedents equals male + female", {
-  expect_true(all(data$total_decedents == data$male + data$female))  
-})
+# Test 7: Check that the total_decedents column matches male + female.
+all(data$total_decedents == data$male + data$female)
 
