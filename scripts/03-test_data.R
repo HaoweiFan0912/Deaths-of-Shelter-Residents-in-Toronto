583965f4ba04127864172f01e51a686a124e99b3
HaoweiFan0912 <fhw0912@gmail.com>
2024-09-22 15:54:12 -0400

    Wrong test numbers were modified
    Wrong test numbers were modified

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -61,21 +61,21 @@ all(data$total_decedents == data$male + data$female)
 # now I will test the actual data.
 data <- read_csv("data/analysis_data/analysis_data.csv")
 
-# Test 1: Ensure year_month format is correct (YYYY-MM)
+# Test 8: Ensure year_month format is correct (YYYY-MM)
 all(grepl("^\\d{4}-\\d{2}$", data$year_month))
 
 
-# Test 2: Ensure year_month is in the right period.
+# Test 9: Ensure year_month is in the right period.
 all(ym(data$year_month) >= ym("2007-03") 
     & ym(data$year_month) <= ym("2024-02"))
 
 
-# Test 3: Ensure all seasons are assigned correctly
+# Test 10: Ensure all seasons are assigned correctly
 valid_seasons <- c("Spring", "Summer", "Autumn", "Winter")
 all(data$season %in% valid_seasons)
 
 
-# Test 4: the actual season matches the expected season
+# Test 11: the actual season matches the expected season
 # Function to determine the correct season based on the month
 get_season <- function(month) {
   if (month %in% 3:5) {
@@ -93,15 +93,15 @@ correct_season <- tibble(correct_season = sapply(data$month, get_season))
 all(correct_season$correct_season == data$season)
 
 
-# Test 5: Check for valid total decedents
+# Test 12: Check for valid total decedents
 all(data$total_decedents >= 0 & # is non-negative?
       data$total_decedents == as.integer(data$total_decedents)) # is integer?
 
-# Test 6: Check for valid 'male'.
+# Test 13: Check for valid 'male'.
 all(data$male >= 0 & # is non-negative?
       data$male == as.integer(data$male)) # is integer?
 
-# Test 7: Check that the total_decedents column matches male + female.
+# Test 14: Check that the total_decedents column matches male + female.
 all(data$total_decedents == data$male + data$female)
 
 
